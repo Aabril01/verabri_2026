f849a754e3cb8069b7f7189dede7d8ed88143ea7
ABRIL <001aabril@gmail.com>
2026-04-17 23:23:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'verabri',
+  webDir: 'www'
+};
+
+export default config;
